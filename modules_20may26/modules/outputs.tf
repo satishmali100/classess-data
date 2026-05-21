794231fcd2e7a5cc282d67492b5a5107ccfe9ab3
Satish Mali <YOUR_GITHUB_EMAIL>
2026-05-21 23:30:41 +0530

    daily auto update

--- modules_20may26/modules/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "dev_rg_name" {
-    value = module.dev_rg.name
-}
-
-output "dev_rg_id" {
-    value = module.dev_rg.id
-  
-}
--- modules_20may26/modules/resource_group/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "name" {}
-variable "location" {}
-variable "tags" {
-    default={}
-}
